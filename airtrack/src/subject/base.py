e391ba6c1518b26cfd5c8deb904d4b2777f36dfe
Chris Karageorgiou Kaneen <ckarageorgkaneen@gmail.com>
2021-08-07 21:19:10 +0300

    Document subject base module

--- airtrack/src/subject/base.py
+++ airtrack/src/subject/base.py
@@ -1,4 +1,15 @@
-"""Interface for the Airtrack subject (mouse)"""
+"""Airtrack subject base module.
+
+This module provides an interface (AirtrackSubject) for querying subject
+(e.g. mouse) information of the Airtrack system.
+
+Example:
+
+    subject = AirtrackSubject()
+    if subject.is_inside_lane():
+        print('Gotcha!')
+    subject.clean_up()
+"""
 import logging
 
 from airtrack.src.camera.base import AirtrackCamera
@@ -17,13 +28,22 @@ handle_camera_error = on_error_raise(
 
 
 class AirtrackSubject:
+    """Airtrack subject information interface."""
+
     def __init__(self):
         self._camera = AirtrackCamera()
 
     @handle_camera_error
     def is_inside_lane(self):
+        """Query subject for being inside or outside the airtable lane.
+
+        :return: ``True`` if the subject is inside the lane,
+            otherwise ``False``
+        :rtype: ``bool``
+        """
         return not self._camera.detect_object(
             AirtrackCameraObject.MOUSE_SIG_1.name)
 
     def clean_up(self):
+        """Clean up the object."""
         self._camera.close()
