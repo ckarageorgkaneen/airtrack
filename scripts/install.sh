9775b094506c60aeea422ba5a68963f7382fa521
Chris Karageorgiou Kaneen <ckarageorgkaneen@gmail.com>
2021-08-08 22:03:43 +0300

    Fix install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,13 +1,21 @@
 #!/usr/bin/env bash
 set -e
 git submodule update --init
-cwd=$(pwd)
+CWD=$(pwd)
+SUBMODULES_TMP_FILE=$(mktemp)
+git config -f .gitmodules -l 				\
+	| grep path 							\
+	| awk '{split($0, a, /=/); print a[2]}' \
+	> $SUBMODULES_TMP_FILE
+PIXY2_PATH=$(grep pixy2 $SUBMODULES_TMP_FILE)
+PYBPOD_API_PATH=$(grep pybpod-api $SUBMODULES_TMP_FILE)
+rm ${SUBMODULES_TMP_FILE}
 # Install pixy2
-cd airtrack/submodules/pixy2
+cd ${PIXY2_PATH}
 ./install.sh
-cd $cwd
+cd ${CWD}
 # Install pybpod-api
-pip install -r airtrack/submodules/pybpod-api/requirements.txt
-pip install -e airtrack/submodules/pybpod-api
+pip install -r "$PYBPOD_API_PATH/requirements.txt"
+pip install -e $PYBPOD_API_PATH
 # Install airtrack
 pip install -e .
