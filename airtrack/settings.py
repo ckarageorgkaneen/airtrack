16a95597c4b631584f3652c54b5f3c48dfa058c0
Chris Karageorgiou Kaneen <ckarageorgkaneen@gmail.com>
2021-08-08 19:24:37 +0300

    Separate aitrack from pybpod settings

---
+++ airtrack/settings.py
@@ -0,0 +1,30 @@
+import os
+import datetime
+import time
+import logging
+from pathlib import Path
+
+# LOGGING
+AIRTRACK_LOG_LEVEL = logging.DEBUG
+
+# SESSIONS
+homepath = os.environ.get('HOMEPATH') or os.environ.get('HOME')
+session_dirname = 'AIRTRACK_SESSIONS'
+session_path = os.path.join(homepath, 'Desktop', session_dirname)
+Path(session_path).mkdir(parents=True, exist_ok=True)
+AIRTRACK_SESSION_PATH = session_path
+AIRTRACK_SESSIONS_LOG_FILE = os.path.join(
+    AIRTRACK_SESSION_PATH, 'airtrack.log')
+AIRTRACK_STREAM_SESSION_TO_STDOUT = False
+
+# FILENAMES
+AIRTRACK_SESSION_NAME = datetime.datetime.fromtimestamp(
+    time.time()).strftime('%Y-%m-%d %H:%M:%S')
+
+# DEVICES
+AIRTRACK_BPOD_SERIAL_PORT = '/dev/ttyACM0'
+
+# PARAMETERS
+AIRTRACK_ACTUATOR_PUSH_TIMEOUT = 3
+# Time the actuator should remain at rest before it is pulled back
+AIRTRACK_ACTUATOR_AT_REST_TIMEOUT = 3
