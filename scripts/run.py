896fae43b45e1a77d46813ddd8108c35e8788222
Chris Karageorgiou Kaneen <ckarageorgkaneen@gmail.com>
2021-08-08 19:26:22 +0300

    Import log level from settings

--- scripts/run.py
+++ scripts/run.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 import logging
+
+from airtrack.settings import AIRTRACK_LOG_LEVEL
+
 from airtrack.src.base import Airtrack
 
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.DEBUG)
+    logging.basicConfig(level=AIRTRACK_LOG_LEVEL)
     Airtrack(emulate=False).run()
