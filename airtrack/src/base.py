0674422268b1524f52ebe41152c2e290fc8d7d7a
Chris Karageorgiou Kaneen <ckarageorgkaneen@gmail.com>
2021-08-09 19:51:43 +0300

    Add _bpod property

--- airtrack/src/base.py
+++ airtrack/src/base.py
@@ -28,16 +28,34 @@ class Airtrack:
     """Airtrack system interface."""
 
     def __init__(self):
-        self._bpod = Bpod(emulator_mode=True)
-        self._bpod.open()
+        self.__bpod = None
+        self._bpod_closed = True
         self._sma = AirtrackStateMachine(self._bpod)
         self._sma_setup()
         # Register exit handler
         atexit.register(self.close)
 
+    @property
+    def _bpod(self):
+        if self.__bpod is None:
+            self._create_bpod()
+        if self._bpod_closed:
+            self._open_bpod()
+        return self.__bpod
+
+    @handle_error
+    def _create_bpod(self):
+        self.__bpod = Bpod(emulator_mode=True)
+
+    @handle_error
+    def _open_bpod(self):
+        self.__bpod.open()
+        self._bpod_closed = False
+
     @handle_error
     def _close(self):
-        self._bpod.close(ignore_emulator=True)
+        self.__bpod.close(ignore_emulator=True)
+        self._bpod_closed = True
 
     @handle_error
     def _run(self):
