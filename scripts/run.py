a738e6d5545f8be63d80b660aaf51050a0d4db1d
Chris Karageorgiou Kaneen <ckarageorgkaneen@gmail.com>
2021-08-26 16:24:50 +0300

    Add optional `trials` argument

--- scripts/run.py
+++ scripts/run.py
@@ -1,11 +1,22 @@
 #!/usr/bin/env python3
 import logging
+import argparse
 
 from airtrack.settings import AIRTRACK_LOG_LEVEL
 
 from airtrack.src import Airtrack
 
-if __name__ == '__main__':
-    logging.basicConfig(level=AIRTRACK_LOG_LEVEL)
+logging.basicConfig(level=AIRTRACK_LOG_LEVEL)
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-t', '--trials', type=int, help='Number of trials.')
+
+
+def run(trials):
     airtrack = Airtrack()
-    airtrack.run()
+    airtrack.run(trials=trials)
+
+
+if __name__ == '__main__':
+    args = parser.parse_args()
+    run(trials=args.trials)
